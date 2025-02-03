35be82c1fdc732161c0fe822e3311c8aa4ea8ea6
zkaplan1103 <zkaplan1103@gmail.com>
2025-02-02 21:36:35 -0800

    startcss

---
+++ client/src/pages/Setup2FA.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Setup2FA = () => {
+    return (
+        <div>
+            return <div>LoginPage</div>;
+        </div>
+    );
+};
+
+export default Setup2FA;
---
+++ client/src/pages/Veryify2FA.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Verify2FA = () => {
+    return (
+        <div>
+            return <div>LoginPage</div>;
+        </div>
+    );
+};
+
+export default Verify2FA;
