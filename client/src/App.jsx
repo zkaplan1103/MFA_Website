aced21dc76cf3db7fbe273b5fd6024a76c55b90e
zkaplan1103 <zkaplan1103@gmail.com>
2025-02-03 11:38:27 -0800

    fixed vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-    return <></>;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold">
+      Hello world!
+    </h1>
+  )
 }
 
-export default App
+
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], 
+  plugins: [react()],
   server: {
     port: 3002,
     open: true,
-  },
-});
+  }
+})
